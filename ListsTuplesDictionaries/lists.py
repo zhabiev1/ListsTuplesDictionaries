1258e3eccfc98125b3bd998f0429a21301456faa
Magomed Zhabiev <m.zhabiev@gmail.com>
2021-12-10 20:08:29 +0200

    List Lesson one

--- ListsTuplesDictionaries/lists.py
+++ ListsTuplesDictionaries/lists.py
@@ -1,3 +1,41 @@
-print("мы изучаем листы пайтона")
-b = [3.0, 3, "Mom", True, [4.0, 2, False, "Dad"]]
-print(b)
+# numbers = [1, 2, 3, 4, 5]
+
+# numbers1 = []
+# numbers2 = list() редкое написание (создание листа как и на 3 строке)
+
+# numbers = [1, 2, 3, 4, 5]
+# numbers2 = list(numbers)
+# numbers3 = numbers
+# print(numbers)
+# print(numbers2)
+# print(numbers3)
+# numbers = [1, 2, 3, 4, 5]
+# print(numbers[0])
+# print(numbers[2])
+# print(numbers[4])
+#
+# test = ["mouse", 3.0, True, 3, ["Cat", 4.0, False, 2], []]
+# print(test[2])
+# print(test[3])
+# print(test[5])
+#
+# numbers[0] = 10
+# print(numbers[0])
+#
+# test[2] = False
+# test[5] = 1
+# print(test)
+
+# numbers = [5] * 6
+# print(numbers)
+
+# numbers = list(range(10))
+# print(numbers)
+
+# number = list(range(3, 10))
+# print(number)
+
+# number1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# number2 = list(range(1, 10))
+# print(number1)
+# print(number2)
