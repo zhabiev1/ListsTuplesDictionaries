6b9f642b28679e3acf8a5fae6f89b76dfe6447d9
Magomed Zhabiev <m.zhabiev@gmail.com>
2021-12-16 16:57:50 +0200

    Tuple lesson

---
+++ ListsTuplesDictionaries/tuples.py
@@ -0,0 +1,54 @@
+# a = ("Maga", 1)
+# print(a)
+
+# user = ("Maga", 1)
+# print(user)
+
+
+# user = ("Maga", )
+# print(user)
+
+
+# user = ["Maga", "Sergey"]
+# user_tuple = tuple(user)
+# print(user_tuple)
+
+# user = ("Maga", "Sergey", "Arnold", "Carolina")
+# print(user[1:3])
+
+# user = ("Maga", 20, False)
+# name, age, married = user
+# print(name, age, married)
+
+# def get_user():
+#     name = "Maga"
+#     age = 22
+#     married = False
+#     return name, age, married
+
+
+# print(get_user())
+
+
+# o = ("Home", 22, True)
+# print(len(o))
+# for i in o:
+#     print(i)
+# i = 0
+# while i < len(o):
+#     print(o[i])
+#     i += 1
+
+
+# user = ("Maga", "Sergey", "Max", "Dima")
+# name = input("Введите имя. которое вы хотите найти: ")
+# if name in user:
+#     print("Имя найдено")
+# else:
+#     print("Такого имени нет")
+
+
+# country = (
+#     ("Ukraine", 3), ("Germany", 2), ("USA", 4)
+# )
+# print(country)
