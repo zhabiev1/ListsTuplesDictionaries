d85a285d977e7af125ff98559b62e0083c95dae2
Magomed Zhabiev <m.zhabiev@gmail.com>
2021-12-27 19:33:59 +0200

    set commit

---
+++ ListsTuplesDictionaries/set.py
@@ -0,0 +1,64 @@
+# users = {"Maga", "Dima", "John", "Robert", "Maga"}  # создаём сет
+# print(users)
+
+# users = ["Maga", "John", "Robert", "Dima", "Dima"]  # создаём лист
+# users2 = set(users)  # меняем лист на сет
+# print(users2)
+
+# users = set()  # создаём  сет
+# print(users)
+
+# users = {"Maga", "John", "Dima", "Ira", "Artem"}  # создаём сет и записываем в него данные
+# print(users)
+# print(type(users))  # выводим тип данных
+# print(len(users))  # выводим длину сета
+
+# print(bin(10))  # десятичные
+
+# users = {"Maga", "John", "Dima", "Ira", "Artem"}  # создам сет
+# a = input("Введите имя, которое хотите удалить: ")  # пользователь вводит
+# if a in users:  # условие
+#     users.remove(a)  # метод для удаления
+# else:
+#     print("такой элемент не найден")  # иначе выводит
+# print(users)
+
+# users = {"Maga", "Dima", "John"}  # создаём сет
+# question = input("Введите имя,которое хотите удалить: ")  # пользователь вводит имя
+# users.discard(question)  # метод для удаления одного значения
+# print(users)
+
+# users = {"Maga", "Dima", "John"}  # создаём сет
+# users.clear()  # метод для очистки сета
+# print(users)
+
+# users = {"Robert", "John", "Humbert"}  # создаём сет
+# for i in users:  # цикл для прогона данных из сета
+#     print(i)
+
+# users = {"Robert", "John", "Humbert"}  # cоздаём сет
+# users2 = users.copy()  # копируеи сет
+# print(users)
+# print(users2)
+
+# users = {"Robert", "John", "Humbert"}  # создаём сет
+# users2 = {"Dima", "Maga", "Kirill"}  # создаём второй сет
+# users3 = users.union(users2) # объединяем сеты
+# print(users)
+# print(users2)
+# print(users3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
