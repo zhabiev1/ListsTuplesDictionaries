2af488caf220ddb49f49c3b6771754b511a88629
Magomed Zhabiev <m.zhabiev@gmail.com>
2022-01-18 16:19:30 +0200

    String lesson three

---
+++ StringLesson/string_lesson_three.py
@@ -0,0 +1,12 @@
+
+# text = "Hello, {first_name}.".format(first_name="Maga")
+# print(text)
+
+# first_name = input(" Whats your name ?")
+# text = f"Hello, {first_name}"
+# print(text)
+
+# info = "Name: {0}\t Age: {1}".format("Maga", 22)
+# print(info)
+
+
