6933dce1474f69ff5eb90de9ed09aba2ff46ca94
Magomed Zhabiev <m.zhabiev@gmail.com>
2022-01-18 15:57:01 +0200

    String lesson two

---
+++ StringLesson/string_lesson_two.py
@@ -0,0 +1,58 @@
+# hello = "hello World"
+# a = input("Введите число: ")
+# if a.isnumeric():  # проверка на число в строке
+#     print("значение числовое")
+#     b = int(a) # конвертируем значение в инт
+# else:
+#     print("Конвертация невозможна ")
+
+# data = "Maga, Zhabiev, 22"
+# question = input("Введите слово :")
+# a = data.startswith(question)  # проверка на стринговые значения в переменной
+# print(a)
+
+# data = "Maga, Zhabiev, 21"
+# question = input("Введите слово: ")
+# a = data.endswith(question)  # проверка на стринговое значение с конца
+# print(a)
+
+
+# string = "   hello  world!    "
+# print(string)
+# string = string.strip()  # удаляем пробелы в начале и в конце
+# print(string)
+
+# print("Iphone 11:", "23 000".rjust(5))  # отступ в строке по значению
+
+# welcome = "Hello World! Goodbye World!"
+# index = welcome.find("ll")  # возвращает индекс элемента
+# print(index)
+
+# index = welcome.find("Wor", 10)  # поиск после 10 индекса
+# print(index)
+
+# phone = "+1-234-567-89-10"
+# edit_phone = phone.replace("-", " ")  # первое значение которое изменяем,а второк на какое изменяем
+# no_space = edit_phone.replace(" ", "") #  меняем перво езначение на второе
+# print(no_space)
+
+
+# text = "Это был огромный, в два обхвата дуб, с обломанными ветвями и с обломанной корой"
+# print(text)
+# edit_text = text.split()  # разделяем строку по пробелам на лист элементов
+# print(edit_text)
+# print(edit_text[6])  # вытягиваем элемент из листа по индексу
+# edit_text_2 = text.split(",")  # разделяем строку по запятым и создаём из этого лист
+# print(edit_text_2)
+
+
+# words = ["Let", "me", "speak", "from", "my", "heart", "in", "English"]
+# print(words)
+# sentence = "/".join(words)  # соединяем элементы листа и ставим между ними слеш
+# print(sentence)
+
+
+# word = "Maga"
+# word_info = "!".join(word)  # раздеяем буквы в строке и ставим между ними восклицательный знак
+# print(word_info)
+
