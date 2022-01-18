a0fc70d7964f471b079487498f2ca78c878e707a
Magomed Zhabiev <m.zhabiev@gmail.com>
2022-01-18 16:51:15 +0200

    random lesson one

---
+++ MainModules/random_lesson_one.py
@@ -0,0 +1,34 @@
+
+import random
+
+# number = random.random()
+# print(number)
+# print(number * 100)
+
+# number = random.randint(10, 15)
+# print(number)
+
+# numbers = random.randrange(1, 10)
+# print(numbers)
+
+# a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(a)
+# random.shuffle(a)
+# print(a)
+# b = random.choice(a)
+# print(b)
+
+
+# numbers = list(range(10, 10001))
+# print(numbers)
+# random.shuffle(numbers)
+# print(numbers)
+# numbers_1 = random.choice(numbers)
+# print(numbers_1)
+# if numbers_1 % 2 == 0:
+#     print("Вы выиграли")
+# else:
+#     print("вы проиграли")
+
+
+
