f964d60e6ccf90a31c4ea608904b4f4d04f11fff
Magomed Zhabiev <m.zhabiev@gmail.com>
2022-01-22 14:24:29 +0200

    math modul

---
+++ MainModules/mathematic.py
@@ -0,0 +1,41 @@
+import math  # библиотека математика
+
+
+# number_a = int(input("Введите первое число : "))
+# number_b = int(input("Введите второе число: "))
+# print(pow(number_a, number_b ))  # возводим в степень
+
+# a = int(input("Введите число а: "))
+# b = int(input("Введите число b: "))
+# print(a**b)  # возводим в степень другим способом
+
+# a = int(input("Введите число: "))
+# print(int(math.sqrt(a)))  # ищем корень от числа
+
+# a = float(input("Введите число: "))
+# print(math.ceil(a))  # округляет в пользу большего числа
+
+# a = float(input("Введите число: "))
+# print(math.floor(a))  # округляет в пользу меньше числа
+
+# a = float(input("Введите число в радианах :"))
+# print(math.degrees(a))  # перевод из радианов в градусы
+
+# a = float(input("Введите число в градусах : "))
+# print(math.radians(a))  # перевод из градусов в радианы
+
+# a = float(input("Введите число в градусах: "))
+# print(math.sin(a))  # синус числа
+
+# a = int(input("Введите число в градусах: "))
+# print(math.cos(math.radians(a)))  # косинус числа
+
+
+# a = int(input("Введите число в градусах : "))
+# print(math.tan(math.radians(a)))  #  вычисление тангенса от числа
+
+# print(math.pi)
+# print(math.e)
+# print(math.inf)
+# print(math.tau)
+
